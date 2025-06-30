29f66046b5daaaeb40ff817cd0b6634e093805cb
Kimball Oyler <167648368+koyler88@users.noreply.github.com>
2025-06-30 13:45:34 -0600

    feat: configure Express with static files, ejs, and base indexRouter
    - Serve static assets from /public
    - set EJS as view engine
    - Enabled URL-encoded body parsing
    - Added base router with indexRouter.js

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 require("dotenv").config();
+const path = require("node:path");
 const express = require("express");
+const indexRouter = require("./routes/indexRouter");
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Testing")
-})
+// static assets
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
 
+// views assets
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
+// Middleware to parse incoming request bodies
+app.use(express.urlencoded({ extended: true }));
 
+// Routes
+app.use("/", indexRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));
