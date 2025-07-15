f6420f0846b88f4ed8ec1b659fa480de5386f146
Kimball Oyler <167648368+koyler88@users.noreply.github.com>
2025-07-15 10:39:28 -0600

    fix: hide login/register for authenticated users

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,7 +2,7 @@ const { Router } = require("express")
 const indexRouter = Router();
 
 indexRouter.get("/", (req, res) => {
-    res.render("index")
+    res.render("index", {user: req.user})
 })
 
 module.exports = indexRouter;
